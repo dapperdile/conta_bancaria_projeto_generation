73bde6a29f661dda486a23f06f013b339c061df6
dapperdile <rafinhawc@hotmail.com>
2025-12-10 18:16:26 -0300

    Criado modulos de procura, atualização e exclusão de contas, da ContaController, e implementado no Menu

--- Menu.ts
+++ Menu.ts
@@ -13,22 +13,23 @@ export function main() {
     let titular: string
     const tiposContas = ['Conta Corrente', 'Conta Poupanca']
 
+    
+    console.log("\nCriar Contas\n");
 
-   // Objeto da Classe ContaCorrente (Teste)
-    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
-    contacorrente.visualizar();
-    contacorrente.sacar(2000);
-    contacorrente.visualizar();
-    contacorrente.depositar(1000);
-    contacorrente.visualizar();
+    let cc1: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000, 100.0);
+    contas.cadastrar(cc1);
 
-    // Objeto da Classe ContaPoupanca (teste)
-    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
-    contapoupanca.visualizar();
-    contapoupanca.sacar(200);
-    contapoupanca.visualizar();
-    contapoupanca.depositar(1000);
-    contapoupanca.visualizar();
+    let cc2: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000, 100.0);
+    contas.cadastrar(cc2);
+
+    let cp1: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000, 12);
+    contas.cadastrar(cp1);
+
+    let cp2: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Juliana Ramos", 8000, 15);
+    contas.cadastrar(cp2);
+
+    contas.listarTodas();
+
 
     while (true) {
 
@@ -104,15 +105,56 @@ export function main() {
             case 3:
                 console.log(colors.fg.whitestrong,"\n\nConsultar dados da Conta - por número\n\n",colors.reset);
 
+                console.log("Digite o número da Conta: ")
+                numero = readlinesync.questionInt("")
+                contas.procurarPorNumero(numero)
+
                 keyPress()
                 break;
             case 4:
                 console.log(colors.fg.whitestrong,"\n\nAtualizar dados da Conta\n\n",colors.reset);
 
+                console.log("Digite o número da Conta: ")
+                numero = readlinesync.questionInt("")
+
+                let conta = contas.buscarNoArray(numero)
+
+                if (conta != null) {
+                    console.log("Digite o Número da agência")
+                    agencia = readlinesync.questionInt("")
+
+                    console.log("Digite o Nome do Titular da conta: ")
+                    titular = readlinesync.question("")
+
+                    tipo = conta.tipo
+
+                    console.log("\nDigite o Saldo da conta (R$)")
+                    saldo = readlinesync.questionFloat("")
+
+                    switch (tipo) {
+                        case 1:
+                            console.log("Digite o Limite da Conta (R$): ")
+                            limite = readlinesync.questionFloat("")
+                            contas.atualizar(new ContaCorrente(numero, agencia, tipo, titular, saldo, limite))
+                            break
+                        case 2:
+                            console.log("Digite o Dia do aniversário da Conta Poupança: ")
+                            aniversario = readlinesync.questionInt("")
+                            contas.atualizar(new ContaPoupanca(numero, agencia, tipo, titular, saldo, aniversario))
+                            break
+                    }
+                } else {
+                    console.log(colors.fg.red, "\nA Conta numero: " + numero + " não foi encontrada!", colors.reset)
+                }
+
                 keyPress()
                 break;
             case 5:
                 console.log(colors.fg.whitestrong,"\n\nApagar uma Conta\n\n",colors.reset);
+
+                console.log("Digite o número da Conta: ")
+                numero = readlinesync.questionInt("")
+                contas.deletar(numero)
 
                 keyPress()
                 break;
--- src/controller/ContaController.ts
+++ src/controller/ContaController.ts
@@ -8,29 +8,53 @@ export class ContaController implements ContaRepository {
 
 
     procurarPorNumero(numero: number): void {
-        throw new Error("Method not implemented.");
+        let buscaConta = this.buscarNoArray(numero)
+
+        if (buscaConta != null) {
+            buscaConta.visualizar()
+        } else
+            console.log(colors.fg.red, "\nA Conta numero: " + numero + " não foi encontrada!", colors.reset)
     }
+
     listarTodas(): void {
         for (let conta of this.listaContas){
             conta.visualizar()
         }
     }
+
     cadastrar(conta: Conta): void {
         this.listaContas.push(conta)
         console.log(colors.fg.greenstrong, "\n Conta número: " + conta.numero + " foi criada com sucesso!", colors.reset)
     }
+
     atualizar(conta: Conta): void {
-        throw new Error("Method not implemented.");
+        let buscaConta = this.buscarNoArray(conta.numero)
+
+        if (buscaConta != null) {
+            this.listaContas[this.listaContas.indexOf(buscaConta)] = conta
+            console.log(colors.fg.greenstrong, "\nA conta numero: " + conta.numero + " foi atualizada com sucesso!", colors.reset)
+        } else
+            console.log(colors.fg.red, "\nA conta numero" + conta.numero + " não foi encontrada!", colors.reset)
     }
+
     deletar(numero: number): void {
-        throw new Error("Method not implemented.");
+        let buscaConta = this.buscarNoArray(numero)
+
+        if (buscaConta != null) {
+            this.listaContas.splice(this.listaContas.indexOf(buscaConta), 1)
+            console.log(colors.fg.greenstrong, "\nA Conta numero: " + numero + " foi apagada com sucesso!", colors.reset)
+        } else
+            console.log(colors.fg.red, "\nA Conta numero: " + numero + " não foi encontrada!", colors.reset)
     }
+
     sacar(numero: number, valor: number): void {
         throw new Error("Method not implemented.");
     }
+
     depositar(numero: number, valor: number): void {
         throw new Error("Method not implemented.");
     }
+
     transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
         throw new Error("Method not implemented.");
     }
@@ -39,4 +63,11 @@ export class ContaController implements ContaRepository {
         return ++ this.numero
     }
 
+    public buscarNoArray(numero: number): Conta | null {
+        for (let conta of this.listaContas) {
+            if (conta.numero === numero)
+                return conta
+        }
+        return null
+    }
 }
